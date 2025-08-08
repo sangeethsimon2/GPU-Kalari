ebb50dd470ba780b66226d21df4be075b2b5a791
sangeeth <sangeethsimon2@gmail.com>
2025-08-08 11:03:19 +0200

    Make func calls type independent

--- CUDA/BVH/src/bvh.cu
+++ CUDA/BVH/src/bvh.cu
@@ -6,19 +6,28 @@
 //Device memory pointer for rectangle objects
 rectangleObject* d_rects;
 
-//TODO: if this method has to accept any type of objects, then the std vector 
-// must store some kind of type erased objects
-void uploadTodevice(rectangleObject* h_rects, const size_t numObj){
-    util::my_uploadToDevice(&d_rects, h_rects, numObj);   
+//Device memory pointer for AABB objects
+AABB* d_aabbs;
+
+template<typename T>
+void uploadTodevice(T* h_rects, const size_t numObj){
+    util::my_uploadToDevice(&d_rects, h_rects, numObj);  // This could be error prone when T! typeof d_rects 
 }
 
-void testdownloadToHost(rectangleObject* h_rects, const size_t num_rects){
-    util::my_testDownloadToHost(h_rects, d_rects, num_rects);
+void testdownloadToHost(rectangleObject* h_rects, const size_t numObjs){
+    util::my_testDownloadToHost(h_rects, d_rects, numObjs);
 }
 
-void computeTree(size_t num_rects, int blocksPerGrid, int threadsPerBlock){
-    computeAABBs<<<blocksPerGrid, threadsPerBlock>>>(num_rects);
-}
-__global__ void computeAABBs(const size_t num_rects){
+template<typename T>
+__global__ void computeAABBs(T* d_rects, const size_t numObjs, AABB* d_aabbs){
      int idx = blockIdx.x * blockDim.x + threadIdx.x;
+     if(idx < numObjs){
+        d_rects[idx].originX=0.;
+     }
 }
+void computeTree(size_t numObjs, int blocksPerGrid, int threadsPerBlock){
+    util::allocateMemoryOndevice(d_aabbs, numObjs, sizeof(AABB));
+    computeAABBs<<<blocksPerGrid, threadsPerBlock>>>(d_rects, numObjs, d_aabbs);
+}
+
+
