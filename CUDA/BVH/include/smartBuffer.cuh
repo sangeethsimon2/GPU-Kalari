8aaf6eb57e9ef4dd299d22da574a2b4303c83d0f
sangeeth <sangeethsimon2@gmail.com>
2025-12-23 07:08:58 +0100

    Obj managing CPU-GPU memory
    - This object encapsulates the pointers for host and device memories.
    - Interfaces with mycudaUtilities.cuh for uploading and downloading data to/from host-device.
    - freeing memory is still the responsibility of the mycudaUtilities kernels.

---
+++ CUDA/BVH/include/smartBuffer.cuh
@@ -0,0 +1,30 @@
+#pragma once 
+
+#include "AABB.h"
+#include "mycudaUtilities.cuh"
+
+namespace SmartMemoryManager{
+
+    template<typename T>
+    struct HostDeviceMemoryManager{
+        public:
+            HostDeviceMemoryManager(T* _ptr, size_t _count): h_ptr(_ptr), m_count(_count){};
+            void uploadToDevice(){
+                util::my_uploadToDevice(&d_ptr, h_ptr, m_count);
+            }
+            void downloadToHost(){
+                util::my_downloadToHost(h_ptr, d_ptr, m_count);
+            }
+            ~HostDeviceMemoryManager(){
+                h_ptr=nullptr;
+                m_count=0.;
+                d_ptr=nullptr; 
+            }
+        private:
+            T* h_ptr=nullptr;
+            T* d_ptr=nullptr;
+            size_t m_count{};
+        
+    };
+
+}
